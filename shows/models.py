635ef15e9c4c9d5c7931d085e74ebda6d80d2561
Gavin Browning <gavin.browning@aggiemail.usu.edu>
2017-04-28 19:53:20 +0000

    Admin.py finished

--- shows/models.py
+++ shows/models.py
@@ -7,7 +7,13 @@ class Show(models.Model):
     show_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.show_text
+
 class Rating(models.Model):
     show = models.ForeignKey(Show, on_delete=models.CASCADE)
     rating_text = models.CharField(max_length=200)
     rate = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.rating_text
