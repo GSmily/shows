3e986e0098793cd57e0dd38924723ac2d585ee40
Gavin Browning <gavin.browning@aggiemail.usu.edu>
2017-04-28 20:10:29 +0000

    added new shows views and urls

--- shows/urls.py
+++ shows/urls.py
@@ -3,5 +3,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ex: /shows/
     url(r'^$', views.index, name='index'),
+
+    # ex: /shows/5/
+    url(r'^(?P<show_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # ex: /shows/5/rating/
+    url(r'^(?P<show_id>[0-9]+)/rating/$', views.rating, name='rating'),
+
+    # ex: /shows/5/rate/
+    url(r'^(?P<show_id>[0-9]+)/rate/$', views.rate, name='rate'),
 ]
--- shows/views.py
+++ shows/views.py
@@ -3,4 +3,14 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponsse('hey')
+
+def detail(request, show_id):
+    return HttpResponse("You're looking at show %s." % show_id)
+
+def rating(request, show_id):
+    response = "You're looking at the rating of show %s."
+    return HttpResponse(response % show_id)
+
+def rate(request, show_id):
+    return HttpResponse("You're rating on show %s." % show_id)
