762a7b25c6a3c075abc9ac08c2a7a5c69195f6c8
Gavin Browning <gavin.browning@aggiemail.usu.edu>
2017-04-28 19:34:39 +0000

    Models added and Migrated

--- shows/models.py
+++ shows/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Show(models.Model):
+    show_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Rating(models.Model):
+    show = models.ForeignKey(Show, on_delete=models.CASCADE)
+    rating_text = models.CharField(max_length=200)
+    rate = models.IntegerField(default=0)
